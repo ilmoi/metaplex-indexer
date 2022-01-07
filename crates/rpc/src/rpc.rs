1a95de561ef1438c7bd24db0654a6926c1a94c0f
raykast <rookie1286@gmail.com>
2022-01-06 17:39:04 -0800

    Started writing docs for the rpc module.

--- crates/rpc/src/rpc.rs
+++ crates/rpc/src/rpc.rs
@@ -24,26 +24,57 @@ fn internal_error<E: Into<indexer_core::error::Error>>(
 }
 
 #[rpc]
+/// API specification for the indexer RPC server
 pub trait Rpc {
+    /// The RPC `getListings` method.
+    ///
+    /// Returns a vector of all indexed listings, showing a brief summary of
+    /// each listing.
     #[rpc(name = "getListings")]
     fn get_listings(&self) -> Result<Vec<Listing>>;
+
+    /// The RPC `getStorefronts` method.
+    ///
+    /// Returns a vector of all indexed storefronts, showing the configured
+    /// storefront data stored on Arweave.
     #[rpc(name = "getStorefronts")]
     fn get_storefronts(&self) -> Result<Vec<Storefront>>;
+
+    /// The RPC `getStoreCount` method.
+    ///
+    /// Returns a count of all indexed storefronts.
     #[rpc(name = "getStoreCount")]
     fn get_store_count(&self) -> Result<i64>;
+
+    /// The RPC `getStoreListings` method.
+    ///
+    /// Returns a vector of all indexed listings associated with the given store
+    /// subdomain.
     #[rpc(name = "getStoreListings")]
     fn get_store_listings(&self, store_domain: String) -> Result<Vec<Listing>>;
+
+    /// The RPC `getListingMetadatas` method.
+    ///
+    /// Returns a vector of all items cached for the given listing address.
     #[rpc(name = "getListingMetadatas")]
     fn get_listing_metadatas(&self, listing_address: String) -> Result<Vec<ListingItem>>;
+
+    /// The RPC `getListingDetails` method.
+    ///
+    /// Returns a more detailed representation of a single listing, with more
+    /// item information and bid history.
     #[rpc(name = "getListingDetails")]
     fn get_listing_details(&self, listing_address: String) -> Result<ListingDetails>;
 }
 
+/// A server implementing the [`Rpc`] spec.
 pub struct Server {
     db_pool: Pool,
 }
 
 impl Server {
+    /// Construct a new server from a database connection pool
+    #[must_use]
     pub fn new(db_pool: Pool) -> Self {
         Self { db_pool }
     }
